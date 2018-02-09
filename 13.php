2fbebd0950b2638375326fb54970337219398768
Maryna Rozdobudko <sobolewskaia@gmail.com>
2018-02-09 15:31:34 +0200

    13

---
+++ 13.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 09.02.2018
+ * Time: 15:20
+ * 13. Вывести таблицу умножения
+ */
+
+for($i = 2; $i<=10; $i++) {
+    for($b = 2; $b <= 10; $b++){
+        echo  "$i * $b =" .$i*$b. "<br>" ;
+    }
+    echo "<br>";}
